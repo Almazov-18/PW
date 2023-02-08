49253a5dbd8ec200a5a8c039b7760077d2f561ee
Yevhen Spilnyk <jksspilnik75@gmail.com>
2023-02-08 12:37:02 +0200

    add a test folder

---
+++ tests/tablePage.spec.ts
@@ -0,0 +1,18 @@
+import { searchTest as test } from './test'
+import userList from '../fixtures/assets/contacts.json'
+const randomItem: any = userList[Math.floor(Math.random() * userList.length)]
+
+test.describe('Run all tests for the table page', async () => {
+    test.beforeEach(async ({ tablePage }) => {
+        await tablePage.openTablePage()
+
+    })
+
+    // test('Verify all users on the main page', async ({ tablePage }) => {
+    //     await tablePage.verifyUserContacts(userList)
+    // })
+
+    test('Verify the search input', async ({ tablePage }) => {
+        await tablePage.verifySearchInput(userList, randomItem)
+    })
+})
